9bccb9f63bec48cb83f5f9661ab5ceb78c3b3098
Daniel Ghirasim <danielghirasim@gmail.com>
2022-04-27 14:42:33 +0300

    fixing some stuff before code blows up

--- src/app/deals/deal-list/deal-item/deal-item.component.ts
+++ src/app/deals/deal-list/deal-item/deal-item.component.ts
@@ -4,14 +4,15 @@ import { Deal } from '../../deal.model';
 @Component({
   selector: 'app-deal-item',
   templateUrl: './deal-item.component.html',
-  styleUrls: ['./deal-item.component.scss']
+  styleUrls: ['./deal-item.component.scss'],
 })
 export class DealItemComponent implements OnInit {
   @Input() deal: Deal;
+  dealOldPrice: string;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.dealOldPrice = this.deal.worth === 'N/A' ? '' : this.deal.worth;
   }
-
 }
